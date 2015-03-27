5063e86500c37352ae00e4b62caf1eec26c0ec54
heavenshell <heavenshell.jp@gmail.com>
2015-03-28 01:23:31 +0900

    Add tests.

--- tests/test_misawa_handler.py
+++ tests/test_misawa_handler.py
@@ -15,7 +15,7 @@ import requests
 from mock import patch
 from unittest import TestCase
 from robo.robot import Robot
-from robo.handlers.misawa import Client
+from robo.handlers.misawa import Client, Misawa
 
 
 def dummy_response(m, filename=None):
@@ -48,10 +48,43 @@ class TestClient(TestCase):
     def setUpClass(cls):
         cls.client = Client()
 
-    def test_generate_url(self):
+    @patch('robo.handlers.misawa.requests.get')
+    def test_generate_url(self, m):
+        """ Client().generate('query') should generate search by keyword. """
+        dummy_response(m, './fixture.json')
+
         ret = self.client.generate('\u30C9\u30E9\u30E0')
         self.assertRaisesRegexp(r'^http://+', ret)
 
-    def test_generate_url_query_is_none(self):
+    @patch('robo.handlers.misawa.requests.get')
+    def test_generate_url_query_is_none(self, m):
+        """ Client().generate(None) should generate random image. """
+        dummy_response(m, './fixture.json')
+
         ret = self.client.generate()
         self.assertRaisesRegexp(r'^http://+', ret)
+
+
+class TestMisawaHandler(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        logger = logging.getLogger('robo')
+        logger.level = logging.ERROR
+        cls.robot = Robot('test', logger)
+
+        misawa = Misawa()
+        misawa.signal = cls.robot.handler_signal
+        method = cls.robot.parse_handler_methods(misawa)
+        cls.robot.handlers.extend(method)
+
+        adapter = NullAdapter(cls.robot.handler_signal)
+        cls.robot.adapters['null'] = adapter
+
+    @patch('robo.handlers.misawa.requests.get')
+    def test_should_lgtm(self, m):
+        """ Misawa().get() should search misawa url. """
+        dummy_response(m, 'fixture.json')
+        self.robot.handler_signal.send('test misawa')
+        self.assertRegexpMatches(self.robot.adapters['null'].responses[0],
+                                 r'^http://*')
+        self.robot.adapters['null'].responses = []
